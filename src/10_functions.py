bb7c5f2c98c6f620611d23c698e4eb472c5b1c55
vanesa-tamu <vahe5495@tamu.edu>
2020-08-07 19:09:04 -0500

    finshed most

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,29 @@
 
 # YOUR CODE HERE
 
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+
+
+num = int(input("Enter a number: "))
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+
+def answer(x):
+    if is_even(x):
+        print("Even!")
+    else:
+        print("Odd")
+
+
+answer(num)
 
